74252848aae18a67cca646647a0095fa05b26d78
dustin95 <dtno1.study@gmail.com>
2022-07-01 21:39:15 +0700

    Add actuator

---
+++ sd-actuator/src/main/java/com/sd/actuator/SdActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.sd.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdActuatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SdActuatorApplication.class, args);
+  }
+
+}
